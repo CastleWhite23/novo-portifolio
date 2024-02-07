f17bb36c07288142b551a75d7bfc12a7cac62e98
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-07 20:08:51 -0300

    importando o home e o projetos no portifolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Home } from "./pages/home/Home"
+import { Projetos } from "./pages/projetos/Projetos"
 
 function App() {
-
-
   return (
     <>
-
+        <Home/>
+        <Projetos/>
     </>
   )
 }
