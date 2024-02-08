4c574b376543598fa088a4f6978d5b867f6cd831
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-07 21:05:41 -0300

    criando compoenente

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
+import './Home.css'
+import { Button } from '../../components/Button/Button'
 
 const Home = () => {
     return (
-      <>
-            <h1>Home</h1>
-      </>
+        <>
+            <div className="home">
+                <div className="logo">whitecastle</div>
+                <div className="apresentacao">
+                    <h1>Pedro Castelo</h1>
+                    <p>Sou designer web e desenvolvedor freelancer, seja bem vindo ao meu portifólio :)</p>
+                    <Button text='contato' variant='contato'/>
+                </div>
+                <div className="sidebar">
+                    <div className="bg"></div>
+                    <div className="sidebar-itens">
+                        <div className="sidebar-item">Destaques</div>
+                        <div className="sidebar-item">Habilidades</div>
+                        <div className="sidebar-item">Sobre mim</div>
+                        <div className="sidebar-item">Contatos</div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
-  }
-  
-  export {Home}
-  
+}
+
+export { Home }
