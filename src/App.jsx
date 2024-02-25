381cb48225b879f9e8b3c4b16c77587565e16a97
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-25 15:15:42 -0300

    adicionando os componentes de pagina em um container principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Home } from "./pages/home/Home"
 import { Projetos } from "./pages/projetos/Projetos"
 import { Navbar } from "./components/navbar/Navbar"
+import { PageComponent } from "./pages/PageComponent/PageComponent"
 
 function App() {
   return (
     <>
         {/* <Home/>  */}
         <Navbar/>
-        <Projetos/>
+        <PageComponent>
+          <Projetos />
+
+        </PageComponent>
        
     </>
   )
