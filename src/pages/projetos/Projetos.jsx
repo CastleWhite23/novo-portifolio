09883bc808edc40254a310f2ca8d7111ae0e330f
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-25 15:20:02 -0300

    adicionando cards na pagina de projetos

--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -1,15 +1,30 @@
 import './Projetos.css'
+import { Card } from '../../components/Card/Card'
+
+
 
 const Projetos = () => {
-    return (
-      <>
-          <div className="projetos">
-              <h1>Trabalhos principais</h1>
+  return (
+    <>
+        <div className="projetos">
+          <h1 className='titulo-pagina'>Trabalhos principais</h1>
+          <div className="cards-principais">
+            <Card titulo={'Projeto X'} width={'380px'} height={'350px'} variant={'home'} />
+            <Card titulo={'Projeto X'} width={'380px'} height={'350px'} variant={'home'}
+              style={{
+                marginTop: '-100px'
+              }} />
+            <Card titulo={'Projeto X'} width={'380px'} height={'350px'} variant={'home'}
+              style={{
+                marginTop: '-200px'
+              }}
+            />
           </div>
-          
-      </>
-    )
-  }
-  
-  export {Projetos}
-  
+        </div>
+
+
+    </>
+  )
+}
+
+export { Projetos }
