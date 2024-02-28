018b2e6ece80e2b060bca5c169a55a01d22121a0
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-02-28 16:57:17 -0300

    estilizando e identando código da pagina dos destaques

--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -4,23 +4,40 @@ import { Card } from '../../components/Card/Card'
 
 
 const Projetos = () => {
+
+
+
+
   return (
     <>
-        <div className="projetos">
-          <h1 className='titulo-pagina'>Trabalhos principais</h1>
-          <div className="cards-principais">
-            <Card titulo={'Projeto X'} width={'380px'} height={'350px'} variant={'home'} />
-            <Card titulo={'Projeto X'} width={'380px'} height={'350px'} variant={'home'}
-              style={{
-                marginTop: '-100px'
-              }} />
-            <Card titulo={'Projeto X'} width={'380px'} height={'350px'} variant={'home'}
-              style={{
-                marginTop: '-200px'
-              }}
-            />
-          </div>
+      <div className="projetos">
+        <h1 className='titulo-pagina'>Trabalhos principais</h1>
+        <div className="cards-principais">
+          <Card
+            titulo={'Projeto X'}
+            width={'380px'}
+            height={'350px'}
+            variant={'home'}
+          />
+          <Card
+            titulo={'Projeto X'}
+            width={'380px'}
+            height={'350px'}
+            variant={'home'}
+            style={{
+              marginTop: '-100px'
+            }} />
+          <Card
+            titulo={'Projeto X'}
+            width={'380px'}
+            height={'350px'}
+            variant={'home'}
+            style={{
+              marginTop: '-200px'
+            }}
+          />
         </div>
+      </div>
 
 
     </>
