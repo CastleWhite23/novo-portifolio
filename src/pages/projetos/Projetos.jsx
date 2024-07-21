197e49ba32a2635a8a7d3303c4cd80a28d7783b5
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-07-21 16:25:23 -0300

    alterando titulos imgs e links dos projetos

--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -1,5 +1,8 @@
 import './Projetos.css'
 import { Card } from '../../components/Card/Card'
+import cbImg from '../../assets/imgs/cbImg.png'
+import pokedex from '../../assets/imgs/pokedex.png'
+import todo from '../../assets/imgs/todo.png'
 
 
 
@@ -14,24 +17,32 @@ const Projetos = () => {
         <h1 className='titulo-pagina'>Trabalhos principais</h1>
         <div className="cards-principais">
           <Card
-            titulo={'Projeto X'}
+            titulo={'CBPATIO'}
             width={'380px'}
             height={'350px'}
             variant={'home'}
+            bgImage={cbImg}
+            site={'https://cbpatio.com'}
+            github={'https://github.com/CastleWhite23/cbpatio_client/'}
           />
           <Card
-            titulo={'Projeto X'}
+            titulo={'Pokedex'}
             width={'380px'}
             height={'350px'}
             variant={'home'}
+            bgImage={pokedex}
+            site={'https://pokedex-vert-seven.vercel.app'}
+            github={'https://github.com/CastleWhite23/cbpatio_client/'}
             style={{
               marginTop: '-100px'
             }} />
           <Card
-            titulo={'Projeto X'}
+            titulo={'TodoList'}
             width={'380px'}
             height={'350px'}
             variant={'home'}
+            site={'https://todo-list-react-lake.vercel.app'}
+            bgImage={todo}
             style={{
               marginTop: '-200px'
             }}
