be5bb15c9ee4e90280a586a23611f7264830552a
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-07-23 13:19:12 -0300

    adicionando nova pgina

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { Navbar } from "./components/navbar/Navbar"
 import { PageComponent } from "./pages/PageComponent/PageComponent"
 import { Sobre } from "./pages/sobre/Sobre"
 import { Footer } from "./components/Footer/Footer"
+import { AllProjetos } from "./pages/AllProjetos/AllProjetos"
 
 function App() {
   return (
     <>
-        {/* <Home/>  */}
-        <Navbar/>
-        <PageComponent>
-          <Projetos />
-          <Sobre />
-        </PageComponent>
-        <Footer />
-       
+      {/* <Home/>  */}
+      <Navbar />
+      <PageComponent>
+        <Projetos />
+        <Sobre />
+        <AllProjetos />
+      </PageComponent>
+      <Footer />
+      
+      
+
     </>
   )
 }
