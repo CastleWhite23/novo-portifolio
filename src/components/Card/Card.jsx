31e6d890baaf564f2d9daab427ad938f2f2cb6c0
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-07-24 16:12:20 -0300

    colocando um if pro hover

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,47 +1,63 @@
 
 import './Card.css'
 import { Button } from '../Button/Button'
+import { useState } from 'react'
 
-const Card = ({ width, height, variant, titulo, style, onMouseEnter, onMouseLeave, children, bgImage, site, github, position}) => {
+const Card = ({ width, height, variant, titulo, style, children, bgImage, site, github, position }) => {
 
     const cardStyle = {
         width: width,
         height: height,
         ...style
     }
+    let bgImageStyle = {}
+    variant == 'all' ?
 
-    const bgImageStyle = {
-        backgroundImage: `url(${bgImage})`,
-        backgroundPosition: 'center',
-        width: '100%',
-        height: '100%',
-        backgroundSize: 'cover',
-        borderRadius: '8px 8px 0px 0px',
-      }
+        bgImageStyle = {
+            position: 'absolute',
+            backgroundImage: `url(${bgImage})`,
+            backgroundPosition: 'center',
+            width: '100%',
+            height: '100%',
+            backgroundSize: 'cover',
+            borderRadius: '8px'
+        }
+        :
+        bgImageStyle = {
 
+            backgroundImage: `url(${bgImage})`,
+            backgroundPosition: 'center',
+            width: '100%',
+            height: '100%',
+            backgroundSize: 'cover',
+            borderRadius: '8px 8px 0px 0px',
+        }
 
+    const [hidden, setHidden] = useState(false);
 
     return (
         <>
-            <div className={`card ${variant} ${position}`} style={cardStyle} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-                <div style={bgImage && { ...bgImageStyle}}></div>
+            <div className={`card ${variant} ${position}`} style={cardStyle} onMouseEnter={() => setHidden(true)}
+                    onMouseLeave={() => setHidden(false)} >
+                <div style={bgImage && { ...bgImageStyle }}></div>
+                <div className={`mascara `}
+                    ></div>
 
-                
-                <div className="info">
+                <div className={`info ${!hidden ? '' : 'esconder'}`}>
                     <h2>{titulo}</h2>
                     {children}
-                
-                {
-                    variant != "journey" && (
-                        <div className="btns none" >
-                            <a href={site} target='_blank'><Button text={'site'} variant={'card'} /></a>
-                            <a href={github} target='_blank'><Button text={'github'} variant={'card'} /></a>
-                            {/* <Button text={'mais'} variant={'card'} /> */}
-                        </div>
-                    )
-                }
+
+                    {
+                        variant != "journey" && (
+                            <div className="btns none" >
+                                <a href={site} target='_blank'><Button text={'site'} variant={'card'} /></a>
+                                <a href={github} target='_blank'><Button text={'github'} variant={'card'} /></a>
+                                {/* <Button text={'mais'} variant={'card'} /> */}
+                            </div>
+                        )
+                    }
                 </div>
-                
+
             </div>
         </>
     )
